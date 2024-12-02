172932a8f68fafe972d57068f5da8e938b8babb8
Sara <sarailic431@gmail.com>
2024-12-02 20:31:22 +0100

    closure

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const images = document.querySelectorAll("img");
 let playerScore = 0;
 let computerScore = 0;
 
-let playerChioce;
+// let playerChioce;
 let computerChoice;
 
 const maxScore = 3;
@@ -37,7 +37,7 @@ document.addEventListener("keydown", function (e) {
 
 const play = function () {
   if (
-    (playerChioce === "rock" && computerChoice === "scissors") ||
+    (game.getPlayerChoice() === "rock" && computerChoice === "scissors") ||
     (playerChioce === "paper" && computerChoice === "rock") ||
     (playerChioce === "scissors" && computerChoice === "paper")
   ) {
@@ -58,50 +58,57 @@ const play = function () {
   }
 };
 
-const game = function () {
+const gameCreator = function () {
   const choices = ["rock", "paper", "scissors"];
+  let compChoice = "";
+  let playerChioce = "";
+  let compScore = 0;
+  let playerScore = 0;
 
-  const getCompChoice = () =>
-    choices[Math.trunc(Math.random() * choices.length)];
-  const setPlayerChoice = (e) => e.target.dataset.choice;
-  return { getCompChoice, setPlayerChoice };
+  const setCompChoice = () =>
+    (compChoice = choices[Math.trunc(Math.random() * choices.length)]);
+  const setPlayerChoice = (value) => (playerChioce = value);
+  const getPlayerChoice = () => playerChioce;
+  const getCompChoice = () => compChoice;
+
+  return { getPlayerChoice, setPlayerChoice, setCompChoice, getCompChoice };
 };
 
-const gamePlay = game();
-
+const game = gameCreator();
+// const ui = uiControlerCreator()
 images.forEach(function (el, i) {
   el.addEventListener("click", function (e) {
-    playerChioce = gamePlay.setPlayerChoice(e);
+    // playerChioce = gamePlay.setPlayerChoice(e);
+    game.setPlayerChoice(e.target.dataset.choice);
 
+    game.setCompChoice();
+    console.log(game.getPlayerChoice());
+    console.log(game.getCompChoice());
     // game.setCompChoice(compChoice.getChoice())
-    computerChoice = gamePlay.getCompChoice();
-    // computerChoice = choices[Math.trunc(Math.random() * choices.length)];
-    // console.log(computerChoice);
-    if (gamePlay.setPlayerChoice(e) === gamePlay.getCompChoice()) {
-      infoText.textContent = `You both choose ${gamePlay
-        .getCompChoice()
-        .toUpperCase()}!`;
-    }
 
-    play();
+    // if (gamePlay.setPlayerChoice(e) === gamePlay.getCompChoice()) {
+    //   infoText.textContent = `You both choose ${gamePlay
+    //     .getCompChoice()
+    //     .toUpperCase()}!`;
+    // }
 
-    if (playerScore === maxScore || computerScore === maxScore) {
-      if (playerScore === maxScore) {
-        winner.textContent = "You won the game!";
-        winner.style.color = "green";
-      } else {
-        winner.textContent = "You lost the game!";
-        winner.style.color = "red";
-      }
-      toggleOverlay();
-      winner.classList.remove("hidden");
-      startGame.textContent = "TRY AGAIN";
+    // if (playerScore === maxScore || computerScore === maxScore) {
+    //   if (playerScore === maxScore) {
+    //     winner.textContent = "You won the game!";
+    //     winner.style.color = "green";
+    //   } else {
+    //     winner.textContent = "You lost the game!";
+    //     winner.style.color = "red";
+    //   }
+    //   toggleOverlay();
+    //   winner.classList.remove("hidden");
+    //   startGame.textContent = "TRY AGAIN";
 
-      infoText.textContent = "First who reaches score of three wins the game!";
-      playerScore = 0;
-      computerScore = 0;
-      playerScoreEl.textContent = 0;
-      computerScoreEl.textContent = 0;
-    }
+    //   infoText.textContent = "First who reaches score of three wins the game!";
+    //   playerScore = 0;
+    //   computerScore = 0;
+    //   playerScoreEl.textContent = 0;
+    //   computerScoreEl.textContent = 0;
+    // }
   });
 });
